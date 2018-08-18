e26eefe8b1481a21a87b78ff35e88a1d8662f047
Gagan <gagan_gupta@live.in>
2018-08-18 13:55:53 +0530

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 let app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+server.listen(port, () => {
     console.log(`Started up at ${port}`)
 });
 
